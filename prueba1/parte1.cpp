f570dca523e301288d43a9bbb341c7e499e6e2d0
JuanJoseTJ29 <50315174+JuanJoseTJ29@users.noreply.github.com>
2020-07-14 12:11:05 -0500

    Update parte1.cpp

--- prueba1/parte1.cpp
+++ prueba1/parte1.cpp
@@ -1,11 +1,11 @@
-/*Diseñe un algoritmo que permita ingresar en un vector los nombres de las 
-facultades de la UNMSM, luego por cada facultad deberá ingresar los nombres de las
-escuelas profesionales. En base a esta información realizar lo siguiente:
+/*DiseÃ±e un algoritmo que permita ingresar en un vector los nombres de las 
+facultades de la UNMSM, luego por cada facultad deberÃ¡ ingresar los nombres de las
+escuelas profesionales. En base a esta informaciÃ³n realizar lo siguiente:
 
 a) Leer los datos.
-b) Mostrar los datos leídos.
+b) Mostrar los datos leÃ­dos.
 c) Indicar la facultad que tiene el mayor numero de escuelas.
-d) Mostrar la información calculada en el punto C
+d) Mostrar la informaciÃ³n calculada en el punto C
 */
 
 #include<iostream>
@@ -13,6 +13,56 @@ d) Mostrar la información calculada en el punto C
 using namespace std;
 #define N 100
 
+void leer(int n, int m[], char esc[][N][N], char nom[][N])
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Ingresar nombre de la facultad #"<<i+1<<": ";
+		fflush(stdin);
+		cin.getline(nom[i],N);
+		cout<<"Ingresar cantidad de escuelas de la facultad de "<<nom[i]<<": ";
+		cin>>m[i];
+		for(int j=0;j<m[i];j++)
+		{
+			cout<<"Ingrese nombre de la escuela #"<<j+1<<": ";
+			fflush(stdin);
+			cin.getline(esc[i][j],N);
+		}
+		cout<<"------------------------------------------------\n";
+	}
+}
+
+void mostrar(int n, int m[], char esc[][N][N], char nom[][N])
+{
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Escuelas de la facultad de "<<nom[i]<<":"<<endl<<endl;
+		for(int j=0;j<m[i];j++)
+		{
+			cout<<esc[i][j]<<endl;;
+		}
+		cout<<"------------------------------------------------\n";
+	}
+}
+
+void mayorEscuelas(int n, int m[], char esc[][N][N], char nom[][N])
+{
+	char fm[N];
+	int may=-1;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m[i];i++)
+		{
+			if(may<m[i])
+			{
+				may=m[i];
+				strcpy(fm,nom[i]);
+			}
+		}
+	}
+	cout<<"Facultad con mayor # de facultades es "<<fm<<
+	" con "<<may<<" facultades\n";
+}
 
 void mostrarEscuelas(int n, int m[], char esc[][N][N], char nom[][N])
 {
@@ -21,5 +71,3 @@ void mostrarEscuelas(int n, int m[], char esc[][N][N], char nom[][N])
 		cout<<"Facultad de "<<nom[i]<<" tiene "<<m[i]<<" escuelas\n";
 	}
 }
-
-
